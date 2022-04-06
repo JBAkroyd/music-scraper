535b7302b72a8968ab8bc0e29964f6d4fef75d2e
Jordan Akroyd <jordan.akroyd@gmail.com>
2022-04-06 20:47:06 +1200

    working implementation with no alerting

--- music-scaper.py
+++ music-scaper.py
@@ -1,3 +1,4 @@
+import time
 from unicodedata import name
 from numpy import equal
 from selenium import webdriver
@@ -6,9 +7,9 @@ from selenium.webdriver.common.by import By
 from multiprocessing import Process
 
 class SongData:
-    def __init__(self, title, artist):
-        self.title = title
-        self.artist = artist
+    def __init__(self, title: str, artist: str):
+        self.title = title.lower()
+        self.artist = artist.lower()
 
     def __eq__(self, other: 'SongData'):
         if isinstance(other, SongData):
@@ -16,46 +17,40 @@ class SongData:
         return False
     
     def Print(self):
-        print(f"Title:\t{self.title}\t\t\t\tArtist:\t{self.artist}")
+        print(f"Title: {self.title} Artist: {self.artist}")
 
-def list_songs(song_list):
+def list_songs():
     for x in song_list:
         x.Print()
     print("---")
 
-# def append_song(song, song_list):
-#     if song not in song_list:
-#         song_list.append(song)
-#     else:
-#         print(f"{song} is a duplicate!")
+# appends a song to the song_list
+def append_song(song: SongData):
+    if song not in song_list:
+        song_list.append(song)
+    else:
+        print(f"Title: {song.title} Artist: {song.artist} is a duplicate!")
     
 
+def monitor_songs(driver):
+    music_player = driver.find_element(by=By.CLASS_NAME, value='c-NavPlayer-playing')
+    music_player_text = music_player.text.split('\n')
+    song = SongData(music_player_text[0], music_player_text[1])
+    current_song = song_list[-1] if len(song_list) > 0 else SongData("","")
+    if current_song != song:
+        current_song = song
+        append_song(current_song)
 
-def monitor_songs(song_list):
+if __name__ == '__main__':
+    global song_list
+    song_list = []
+    # configure web driver to scrape javascript webpage
     firefox_options = Options()
     firefox_options.add_argument('--headless')
     url = "https://www.therock.net.nz/home.html"
     driver = webdriver.Firefox(options=firefox_options)
     driver.get(url)
-    current_song = SongData("", "")
     while True:
-        music_player = driver.find_element(by=By.CLASS_NAME, value='c-NavPlayer-playing')
-        music_player_text = music_player.text.split('\n')
-        song = SongData(music_player_text[0], music_player_text[1])
-        if current_song != song:
-            current_song = song
-            # move the append to a thread instead of in this method to improve performance
-            # p1 = Process(target=append_song, args=(current_song, song_list,))
-            # p1.start()
-            # p1.join()
-            if current_song not in song_list:
-                song_list.append(current_song)
-            else:
-                print(f"{current_song} is a duplicate!")
-            p2 = Process(target=list_songs, args=(song_list,))
-            p2.start()
-            p2.join()
-
-if __name__ == '__main__':
-    song_list = []
-    monitor_songs(song_list)
+        monitor_songs(driver)
+        list_songs()
+        time.sleep(10)
